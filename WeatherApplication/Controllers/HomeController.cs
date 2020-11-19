ac335ae9e3844b832a12ac1aeee4baeb57228573
chris <chris.mcshane18@yahoo.com>
2020-11-20 00:38:41 +0100

    Add project files.

---
+++ WeatherApplication/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WeatherApplication.Models;
+
+namespace WeatherApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Establishes a connection with the API and retrieves the weather data.
+        /// CHecks for a successful connection before processing data.
+        /// Converts data to a readable json format, passes content back to javascript call.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<Root> GetWeatherData()
+        {
+            HttpClient client = new HttpClient();
+            Root weatherData = new Root();
+            client.BaseAddress = new Uri("https://www.metaweather.com/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("https://www.metaweather.com/api/location/44544/");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                weatherData = JsonConvert.DeserializeObject<Root>(responseBody);
+            }
+
+            return weatherData;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherApplication/wwwroot/js/site.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    populateWeatherData(1);
+});
+
+//Ajax call to the server method to retrieve the data from the MetaWeather API.
+function populateWeatherData(value) {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetWeatherData",
+        dataType: "json",
+        success: function (data) {
+            //A check for existing elements to be erased and replaced with updated data.
+            if (value === 0) {
+                $("#weatherData").empty();
+            }
+            for (var i = 0; i <= data.consolidated_weather.length - 1; i++)
+            {
+                //Create an element for each weather object.
+                var model = data.consolidated_weather[i];
+                $("#weatherData").append(
+                    '<div class="weatherDiv" style="float:left; width: 32%; height: 10%; margin-top:5%"><h5> Date: ' + model.applicable_date + '</h5><h6>' + model.weather_state_name + '</h6><h6>Temp: ' + Math.round(model.the_temp) + '</h6><p><img src="https://www.metaweather.com/static/img/weather/png/' + model.weather_state_abbr + '.png" style="height: 50px; width: 50px" /></p></div>'
+                );
+            }
+        },
+        error: function (e) {
+            alert("Problem with retrieving data");
+        }
+    });
+}
+
+//A function to initialise the pull to load mechanic
+window.onload = function () {
+    WebPullToRefresh.init({
+        loadingFunction: loadingFunction
+    });
+};
+
+//Runs the refresh of data on the pull down function
+var loadingFunction = function () {
+    return new Promise(function (resolve, reject) {
+        
+        populateWeatherData(0);
+        
+        if (true /* if the loading worked */) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+};
